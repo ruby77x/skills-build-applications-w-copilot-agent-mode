876fb45e102e2a2a1ffbe66c51b77ca900b2ec8d
Yaqouta Alsughier <ruby200731@gmail.com>
2026-02-28 13:39:03 +0000

    done

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-v70n+b7*ojpaogid@%@x+6c$dkv7a9pph+@ulsj76-lgsik@uq
 DEBUG = True
 
 
-# Allow all hosts for development
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(CODESPACE_NAME)
 
 
 # Application definition
